6824c508270d092dc818b3c97f14cafe3d97cad3
Nikhil Suri <nikhilsuri@comcast.net>
2017-12-27 00:36:19 -0800

    fade in/out

--- static/script.js
+++ static/script.js
@@ -1,8 +1,12 @@
-// Highlight current page in navbar
 $(document).ready(function() {
-  // get current URL path and assign 'active' class
+  // Page transition
+  // https://www.abeautifulsite.net/a-clean-fade-in-effect-for-webpages
+  $(function() {
+    $('#mother').removeClass('fade-out');
+  });
+  // Highlight current page in navbar
+  // Get current URL path and assign 'active' class
   var pathname = window.location.pathname;
-  console.log(pathname);
   $('.navbar-nav > a[href="'+pathname+'"]').addClass('active');
   $('.navbar-nav > a[href="'+pathname+'"]')
     .append("<span class='sr-only'>(current)</span>");
